fe1476b8011a0fd1204cb19ff425489750b8eae4
Lbarto12 <Lbarto12@hotmail.com>
2023-12-09 03:56:18 -0500

    Added Day 9

---
+++ Day 9/main.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+with open('input.txt') as file:
+    data: List[List[int]] = [[int(num) for num in line.split()] for line in file.readlines()]
+
+
+
+def extrapolate(seq: List[int], idx: int, sign: int) -> int:
+    diffs: List[int] = [seq[i + 1] - num for i, num in enumerate(seq[:-1])]
+    return 0 if all(diff == 0 for diff in seq) else seq[idx] + sign * extrapolate(diffs, idx, sign)
+
+
+
+print(f'Part 1: {sum(extrapolate(i, idx=-1, sign=1) for i in data)}')
+print(f'Part 2: {sum(extrapolate(i, idx=0, sign=-1) for i in data)}')
+
