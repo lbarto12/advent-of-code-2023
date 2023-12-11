b33c48245bc80b98467cacd69d69749210c28d53
Lbarto12 <Lbarto12@hotmail.com>
2023-12-10 22:11:41 -0500

    Day 6 - Refactored and Added Types

--- Day 6/main.py
+++ Day 6/main.py
@@ -1,25 +1,26 @@
-import math
+from typing import List, Tuple
+
+
+with open('input.txt') as file:
+    raw_records: List[str] = file.readlines()
+    records: List[Tuple[int, int]] = list(zip(*[[int(n) for n in i.split()[1:]] for i in raw_records]))
+
 
 def quadratic(b, c):
     return (-b + (b ** 2 - 4 * c) ** 0.5) / 2, (-b - (b ** 2 - 4 * c) ** 0.5) / 2
 
 
 # Part 1
-with open('input.txt') as file:
-    records = list(zip(*[[int(n) for n in i.split()[1:]] for i in file.readlines()]))
+count: int = 1
+for time, distance in records:
+    a, b = quadratic(time, distance)
+    count *= abs(int(a + 1) if a.is_integer() else int(a) - int(b))
 
-    count = 1
-    for time, distance in records:
-        a, b = quadratic(time, distance)
-        count *= abs(int(a + 1) if a.is_integer() else int(a) - int(b))
-
-    print(f'Part 1: {count}')
+print(f'Part 1: {count}')
 
 
 # Part 2
-with open('input.txt') as file:
-    a, b = quadratic(*[int(''.join(i.split()[1:])) for i in file.readlines()])
-
-    print(f"Part 2: {abs(int(a + 1) if a.is_integer() else int(a) - int(b))}")
+a, b = quadratic(*[int(''.join(i.split()[1:])) for i in raw_records])
+print(f"Part 2: {abs(int(a + 1) if a.is_integer() else int(a) - int(b))}")
 
 
