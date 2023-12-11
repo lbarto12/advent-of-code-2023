6a4e859e6f84c2ddd35d23e136f2d1fd00652723
Lbarto12 <Lbarto12@hotmail.com>
2023-12-10 21:03:47 -0500

    Day 3 - Added Types

--- Day 4/main.py
+++ Day 4/main.py
@@ -1,10 +1,13 @@
 import re
+from typing import List
 
 with open('input.txt') as file:
-    cards = file.readlines()
+    cards: List[str] = file.readlines()
 
 def calculate_card(_card: str) -> int:
-    winning, nums = ({int(n) for n in i.split()} for i in re.match(r'Card *\d+: ([\d ]+) \| ([\d ]+)', _card).groups())
+    winning, nums = (
+        {int(n) for n in _nums.split()} for _nums in re.match(r'Card *\d+: ([\d ]+) \| ([\d ]+)', _card).groups()
+    )
     return len(winning.intersection(nums))
 
 
@@ -13,7 +16,7 @@ print(f'Part 1: {sum(int((1 << calculate_card(card)) / 2) for card in cards)}')
 
 
 # Part 2
-counts = [1] * len(cards)
+counts: List[int] = [1] * len(cards)
 for i, card in enumerate(cards):
     for j in range(i + 1, i + calculate_card(card) + 1):
         counts[j] += counts[i]
