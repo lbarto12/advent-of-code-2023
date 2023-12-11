8862f2303afa8904201cae52a3293d02939fe4a1
Lbarto12 <Lbarto12@hotmail.com>
2023-12-10 19:27:55 -0500

    Day 2 - Reduced Code and Added Types

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,32 +1,32 @@
 import re
+from typing import List, Dict
+
+with open('input.txt') as file:
+    games: List[str] = file.readlines()
 
 # Part 1
-with open('input.txt') as games:
-    sm = 0
-    limits = {
-        "red": 12,
-        "green": 13,
-        "blue": 14
-    }
-    for game_id, game in enumerate(games.readlines()):
-        valid = True
-        for color, limit in limits.items():
-            all_draws = re.findall(rf'(\d+) {color}', game)
-            if any(int(i) > limit for i in all_draws):
-                valid = False
-        sm += valid * (game_id + 1)
-    print(f'Part 1: {sm}')
+sm: int = 0
+limits: Dict[str, int] = {"red": 12, "green": 13, "blue": 14}
+for game_id, game in enumerate(games):
+    valid: bool = True
+    for color, limit in limits.items():
+        all_draws: List[str] = re.findall(rf'(\d+) {color}', game)
+        if any(int(i) > limit for i in all_draws):
+            valid = False
+    sm += valid * (game_id + 1)
+
+print(f'Part 1: {sm}')
 
 
 # Part 2
-with open('input.txt') as games:
-    sm = 0
-    for game in games.readlines():
-        prod = 1
-        for color in ["red", "green", "blue"]:
-            all_draws = list(map(int, re.findall(rf'(\d+) {color}', game)))
-            prod *= max(all_draws)
-        sm += prod
-    print(f'Part 2: {sm}')
+sm = 0
+for game in games:
+    prod: int = 1
+    for color in ["red", "green", "blue"]:
+        all_draws: List[int] = list(map(int, re.findall(rf'(\d+) {color}', game)))
+        prod *= max(all_draws)
+    sm += prod
+
+print(f'Part 2: {sm}')
 
 
