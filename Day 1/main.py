a6ad08f9d852e3e732eed2850343bd109fe23f7e
Lbarto12 <Lbarto12@hotmail.com>
2023-12-10 19:21:57 -0500

    Day 1 - Reduced Code

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,31 +1,34 @@
 import re
+from typing import List, Dict
+
+with open('input.txt') as file:
+    calibration: List[str] = file.readlines()
 
 # Part 1
-with open('input.txt') as file:
-    finals = []
-    for code in file.readlines():
-        nums = ''.join([i for i in code if i.isnumeric()])
-        finals.append(int(nums[0] + nums[-1]))
-    print(f'Part 1: {sum(finals)}')
+finals: List[int] = []
+for code in calibration:
+    nums: str = ''.join([i for i in code if i.isnumeric()])
+    finals.append(int(nums[0] + nums[-1]))
+
+print(f'Part 1: {sum(finals)}')
 
 
 # Part 2
-with open('input.txt') as file:
-    possible = {
-        "one": '1',
-        "two": '2',
-        "three": '3',
-        "four": '4',
-        "five": '5',
-        "six": '6',
-        "seven": '7',
-        "eight": '8',
-        "nine": '9'
-    }
-    finals = []
-    for code in file.readlines():
-        all_nums = re.findall('(?=(' + '|'.join(list(possible) + list(str(i) for i in range(1, 10))) + '))', code)
-        adjusted = [possible.get(i, i) for i in all_nums]
-        finals.append(int(adjusted[0] + adjusted[-1]))
-    print(f'Part 2: {sum(finals)}')
+possible: Dict[str, str] = {
+    "one": '1',
+    "two": '2',
+    "three": '3',
+    "four": '4',
+    "five": '5',
+    "six": '6',
+    "seven": '7',
+    "eight": '8',
+    "nine": '9'
+}
+finals: List[int] = []
+for code in calibration:
+    adjusted: List[str] = [possible.get(i, i) for i in re.findall('(?=(' + '|'.join(list(possible)) + r'|\d))', code)]
+    finals.append(int(adjusted[0] + adjusted[-1]))
+
+print(f'Part 2: {sum(finals)}')
 
